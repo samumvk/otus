2dbd5d0bebdf209aa76bbe76740dbc909af49a36
samum <samumvk@yandex.ru>
2024-01-13 01:42:34 +0700

    Удаление SEQUENCE из первой миграции

--- migrations/Version20240108081617.php
+++ migrations/Version20240108081617.php
@@ -20,8 +20,6 @@ final class Version20240108081617 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('DROP SEQUENCE permissions_id_seq CASCADE');
-        $this->addSql('DROP SEQUENCE role_id_seq CASCADE');
         $this->addSql('CREATE TABLE "user" (id INT NOT NULL, login VARCHAR(255) NOT NULL, password VARCHAR(255) NOT NULL, name VARCHAR(255) NOT NULL, created_at TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL, updated_at TIMESTAMP(0) WITHOUT TIME ZONE DEFAULT NULL, PRIMARY KEY(id))');
         $this->addSql('CREATE INDEX user__name__ind ON "user" (name)');
         $this->addSql('COMMENT ON COLUMN "user".created_at IS \'(DC2Type:datetime_immutable)\'');
@@ -31,8 +29,6 @@ final class Version20240108081617 extends AbstractMigration
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
-        $this->addSql('CREATE SEQUENCE permissions_id_seq INCREMENT BY 1 MINVALUE 1 START 1');
-        $this->addSql('CREATE SEQUENCE role_id_seq INCREMENT BY 1 MINVALUE 1 START 1');
         $this->addSql('DROP TABLE "user"');
     }
 }
